6e26529733bb1a32a301534e6718518249aa6ac0
kavith <kavithkumar9@gmail.com>
2024-01-12 09:16:24 +0530

    comment message

--- routes/blog-routes.js
+++ routes/blog-routes.js
@@ -3,7 +3,7 @@ const { authMiddleware } = require('../middleware/auth')
 const { createBlog, getAllBlogs, findBlogById, updateBlog, updateLikes } = require('../controllers/blogcontroller')
 
 const routes = express.Router()
-
+//ROUTES ARE ADDED BELOW
 routes.post('/', authMiddleware, createBlog)
 routes.get('/', getAllBlogs)
 routes.get('/:id', findBlogById)
